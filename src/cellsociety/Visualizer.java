5d190a78207324716cce4c193e7dc23d4c1f2f7a
Tyler Meier <tkm22@duke.edu>
2020-02-02 14:57:55 -0500

    got the step through button working

--- src/cellsociety/Visualizer.java
+++ src/cellsociety/Visualizer.java
@@ -1,6 +1,5 @@
 package cellsociety;
 
-import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Pos;
@@ -15,11 +14,6 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 
 class Visualizer extends Application {
     public static final int RECTANGLE_SIZE = 30;
@@ -29,9 +23,8 @@ class Visualizer extends Application {
     public static final double PREF_BUTTON_HEIGHT = 100;
     public static final Font titleFont = new Font("Arial", 50);
     public static int FRAMES_PER_SECOND = 1;
-    public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
 
-    private Button mainMenu, pause, resume;
+    private Button mainMenu, pause, resume, stepThrough;
     private Rectangle[][] myGrid;
     private Group group;
 
@@ -50,10 +43,12 @@ class Visualizer extends Application {
         mainMenu = new Button("Main Menu");
         pause = new Button("Pause");
         resume = new Button ("Resume");
+        stepThrough = new Button("Step Through Sim");
 
         mainMenu.setMaxSize(PREF_BUTTON_WIDTH, PREF_BUTTON_HEIGHT);
         pause.setMaxSize(PREF_BUTTON_WIDTH, PREF_BUTTON_HEIGHT);
         resume.setMaxSize(PREF_BUTTON_WIDTH, PREF_BUTTON_HEIGHT);
+        stepThrough.setMaxSize(PREF_BUTTON_WIDTH, PREF_BUTTON_HEIGHT);
         speedUp.setMaxSize(PREF_BUTTON_WIDTH, PREF_BUTTON_HEIGHT);
         slowDown.setMaxSize(PREF_BUTTON_WIDTH, PREF_BUTTON_HEIGHT);
         simButton.setMaxSize(PREF_BUTTON_WIDTH, PREF_BUTTON_HEIGHT);
@@ -62,14 +57,22 @@ class Visualizer extends Application {
         setUpGrid(myCurrSim, mySimFileReader);
 
         mainMenu.setOnAction(e -> myStage.setScene(startScene));
-        pause.setOnAction(e -> animation.stop());
-        resume.setOnAction(e -> animation.play());
+        pause.setOnAction(e -> {
+            animation.stop();
+            stepThrough.setOnAction(ev -> step(myCurrSim)); // when paused, can step through sim
+        });
+        resume.setOnAction(e -> {
+            animation.play();
+            stepThrough.setOnAction(ev -> animation.play()); //when it resumes, can't step through anymore (unsure if animation.play is the right thing to do
+        });
 
-        buttonsVBox.getChildren().addAll(mainMenu, pause, resume, speedUp, slowDown, simButton);
+        buttonsVBox.getChildren().addAll(mainMenu, pause, resume, stepThrough, speedUp, slowDown, simButton);
         buttonsVBox.setAlignment(Pos.CENTER_LEFT);
         BorderPane.setAlignment(nameLabel, Pos.TOP_CENTER);
         BorderPane.setAlignment(group, Pos.CENTER);
         BorderPane boPane = new BorderPane(group, nameLabel, null, null, buttonsVBox);
+
+        animation.play();
 
         Scene scene = new Scene(boPane, width, height, background);
         return scene;
