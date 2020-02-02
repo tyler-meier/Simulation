c50e8c05186387427b560bc1120cc7f4def4874a
FARZEEN NAJAM <fn26@duke.edu>
2020-02-02 00:46:26 -0500

    fire is implemented

---
+++ src/cellsociety/FCELL.java
@@ -0,0 +1,44 @@
+package cellsociety;
+
+import java.util.ArrayList;
+
+public class FCELL {
+    private  int Xrow, Ycol;
+    private FCELL[][] myGrid;
+    protected ArrayList<FCELL> myAdjacentNeighbours = new ArrayList<>();
+    private int Grid_Size;
+    private int type;
+    private double life_time;
+
+    public FCELL(int xPos, int yPos, int state, double prob_catch){
+        xPos= Xrow;
+        yPos = Ycol;
+        state = type;
+        prob_catch = life_time;
+
+    }
+
+    public ArrayList<FCELL> Adjacent_Neighbours() { //all the neighbours of the cell
+        ArrayList<FCELL> myAdjacentNeighbours = new ArrayList<>();
+        if(Ycol<Grid_Size - 1) myAdjacentNeighbours.add(myGrid[Xrow][Ycol+1]); //East
+        if(Ycol>0) myAdjacentNeighbours.add((myGrid[Xrow][Ycol-1])); //West
+        if(Xrow <Grid_Size - 1) myAdjacentNeighbours.add((myGrid[Xrow+1][Ycol]));  //South
+        if(Xrow>0) myAdjacentNeighbours.add((myGrid[Xrow-1][Ycol]));  //North
+
+        return myAdjacentNeighbours;
+    }
+
+    public int getType(){
+
+        return type;
+    }
+
+
+    public void setType(int typo) {
+        this.type = type;
+
+    }
+    public double  getPRobCatch(){
+        return life_time;
+    }
+}
--- src/cellsociety/Fire.java
+++ src/cellsociety/Fire.java
@@ -1,15 +1,65 @@
 package cellsociety;
 
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Fire extends simulation {
+    private FCELL[][] myGrid;
+    public static final int TREE = 1;
+    public static final int BURNING = 2;
+    public static final int EMPTY = 0;
+    private int myGrid_size;
+    private ArrayList<FCELL> neighbours;
 
 
+    private ReadXML reader;
+
+    public Fire(ReadXML myReader) throws ParserConfigurationException, IOException, SAXException {
+        reader = myReader;
+        readFile();
+
+    }
+
+    public void spread(){
+        for(int i = 1; i < myGrid.length-1;i++){  //first and last row do not count
+            for(int j =1; j< myGrid[0].length-1;j++ ){ //first and last col dont count.
+                neighbours = myGrid[i][j].Adjacent_Neighbours();  //GET the neighbours
+                if(myGrid[i][j].getType()== BURNING){  //simply the burning tree dies.
+                    myGrid[i][j].setType(EMPTY);
+                }
+                for( FCELL cell : neighbours){  //for all the neighburs of the current cell
+                    if(cell.getType() == BURNING && myGrid[i][j].getType()==TREE){ //if the neighbours burning, tree
+                        //is present
+                        double random = Math.random(); //generate a number btw 0 and 1
+                        if(random >= myGrid[i][j].getPRobCatch()){ // if the percent is greater than prob catch
+                            myGrid[i][j].setType(BURNING); //it burns
+
+                        }
+
+                        else{
+                            myGrid[i][j].setType(TREE);
+
+                    }
+
+                    }
+
+                }
+
+            }
+
+        }
 
 
+    }
 
 
 
     @Override
     public void update() {
+        spread();
 
     }
 
@@ -19,7 +69,18 @@ public class Fire extends simulation {
     }
 
     @Override
-    public void readFile() {
+    public void readFile() { //updates the grid in the way rules say. the first and last column and the first and last
+        //row are empty.
 
+//        myGrid = new FCELL[][][reader.getRow()][reader.getCol()];
+//        for(int i = 0; i< myGrid.length; i++){
+//            for(int j = 0; j< myGrid[0].length; j++){
+//                myGrid[i][j] = reader.getCell(i, j);
+//            }
+//        }
+//    }
+//
+//    }
     }
+
 }
